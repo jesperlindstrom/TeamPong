33b645dcab33248c8d3e7432e106141f183287f7
Jesper Lindström <raxezdev@gmail.com>
2016-08-15 14:05:32 +0200

    Intent

--- server.js
+++ server.js
@@ -6,96 +6,96 @@ var port = 8000;
 
 app.use('/', express.static(__dirname + '/'));
 server.listen(port, function() {
-	console.log('http listening on ' + port);
+  console.log('http listening on ' + port);
 });
 
 app.get('/reset', function(req, res) {
-	res.send('ok');
-	io.emit('reset');
-	gameSocket = false;
+  res.send('ok');
+  io.emit('reset');
+  gameSocket = false;
 });
 
 app.get('/togglePause', function(req, res) {
-	res.send('ok');
-	gameSocket.emit('togglePause');
+  res.send('ok');
+  gameSocket.emit('togglePause');
 });
 
 var votes = [
-	{
-		up: 0,
-		down: 0,
-		none: 0
-	},
-	{
-		up: 0,
-		down: 0,
-		none: 0
-	}
+{
+  up: 0,
+  down: 0,
+  none: 0
+},
+{
+  up: 0,
+  down: 0,
+  none: 0
+}
 ];
 
 setInterval(function() {
-	if (gameSocket) {
-		gameSocket.emit('tick', [
-			votes[0],
-			votes[1]
-		]);
-	}
+  if (gameSocket) {
+    gameSocket.emit('tick', [
+      votes[0],
+      votes[1]
+      ]);
+  }
 }, 100);
 
 io.on('connection', function(socket){
   socket.on('isGame', function(value) {
-  	handleGame(socket);
+    handleGame(socket);
   });
 
   socket.on('isPlayer', function(team) {
-  	handlePlayer(socket, team);
+    handlePlayer(socket, team);
   })
 });
 
 var gameSocket;
 
 function handleGame(socket) {
-	if (gameSocket) {
-		console.log('game tried to connect, but already existent. Denying new game.');
-		socket.disconnect()
-	}
+  if (gameSocket) {
+    console.log('game tried to connect, but already existent. Denying new game.');
+    socket.disconnect()
+  }
 
-	console.log('game connected');
+  console.log('game connected');
 
-	gameSocket = socket;
+  gameSocket = socket;
 
-	socket.on('disconnect', function() {
-		console.log('game disconnected');
-		gameSocket = false;
-	});
+  socket.on('disconnect', function() {
+    console.log('game disconnected');
+    gameSocket = false;
+  });
 }
 
 function handlePlayer(socket, team) {
-	console.log('player connected to team ' + (team+1));
+  console.log('player connected to team ' + (team+1));
 
-	var lastAction = false;
+  var lastAction = false;
 
-	socket.on('disconnect', function() {
-		if (lastAction != false) {
-			votes[team][lastAction]--;
-		}
-	});
+  socket.on('disconnect', function() {
+    if (lastAction != false) {
+      votes[team][lastAction]--;
+    }
+  });
 
-	socket.on('control', function(value) {
-		if (lastAction != false) {
-			votes[team][lastAction]--;
-		}
+  socket.on('control', function(value) {
+    if (lastAction != false) {
+      votes[team][lastAction]--;
+    }
 
-		var action = false;
-		if (value == false) {
-			action = 'none';
-		} else if (['up', 'down'].indexOf(value) != -1) {
-			action = value;
-		} else {
-			return;
-		}
+    var action = false;
+    if (value == false) {
+      action = 'none';
+    } else if (['up', 'down'].indexOf(value) != -1) {
+      action = value;
+    } else {
+      return;
+    }
 
-		votes[team][action]++;
-		lastAction = action;
-	});
+    votes[team][action]++;
+    lastAction = action;
+  });
 }
